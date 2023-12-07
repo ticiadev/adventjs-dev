031ab2f3f07459e0f47029fe22b81b6c4ac7e3c6
Ticia Francisco <ticiadev@gmail.com>
2023-12-07 14:26:36 -0800

    Add day 07

---
+++ 2023/07.js
@@ -0,0 +1,35 @@
+/*
+generate a drawing of a 3D gift using ASCII characters
+
+The lines of the gifts are drawn with # and the faces with the symbol passed to us as a parameter:
+
+drawGift(4, '+')
+
+
+   ####
+  #++##
+ #++#+#
+####++#
+#++#+#
+#++##
+####
+
+
+drawGift(5, '*')
+
+    #####
+   #***##
+  #***#*#
+ #***#**#
+#####***#
+#***#**#
+#***#*#
+#***##
+#####
+
+
+drawGift(1, '^')
+
+#
+
+*/
